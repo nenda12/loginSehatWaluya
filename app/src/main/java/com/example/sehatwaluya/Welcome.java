bc98b5f9de65009c5d3310a1825069d31853e3cb
nenda12 <56074934+nenda12@users.noreply.github.com>
2019-10-31 14:38:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sehatwaluya/Welcome.java
@@ -0,0 +1,28 @@
+package com.example.sehatwaluya;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Welcome extends AppCompatActivity {
+
+    Button btnExit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        btnExit = (Button) findViewById(R.id.btnExit);
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //perintah untuk mengakhiri aplikasi
+                finish();
+            }
+        });
+
+    }
+}
